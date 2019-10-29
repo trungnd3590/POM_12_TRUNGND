265aa55cc0bff8c6fae02f691a256f97a2b30c52
AD <AD@DESKTOP-EM45AEU>
2019-10-29 14:16:11 +0700

    Abstract Page Commit

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,370 @@
 package commons;
 
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class AbstractPage {
 
+	Select select;
+	Actions actions;
+	WebDriver driver;
+	WebElement element;
+	long shortTimeOut = 5;
+	long longTimeOut = 30;
+	List<WebElement> elements;
+	WebDriverWait waitExplicit;
+	JavascriptExecutor jsExecutor;
+
+	public AbstractPage(WebDriver driver) {
+		this.driver = driver;
+		jsExecutor = (JavascriptExecutor) driver;
+		waitExplicit = new WebDriverWait(driver, longTimeOut);
+		actions = new Actions(driver);
+	}
+
+	public void openUrl(String url) {
+		driver.get(url);
+	}
+
+	public String getPageTitle() {
+		return driver.getTitle().toString();
+	}
+
+	public String getPageCurrentUrl() {
+		return driver.getCurrentUrl();
+	}
+
+	public String getPageSourceCode() {
+		return driver.getPageSource();
+	}
+
+	public void backToPage() {
+		driver.navigate().back();
+	}
+
+	public void forwardToPage() {
+		driver.navigate().forward();
+	}
+
+	public void refreshPage() {
+		driver.navigate().refresh();
+	}
+
+	public void waitForAlertPresence() {
+		waitExplicit.until(ExpectedConditions.alertIsPresent());
+	}
+
+	public void acceptAlert() {
+		driver.switchTo().alert().accept();
+	}
+
+	public void cancelAlert() {
+		driver.switchTo().alert().dismiss();
+	}
+
+	public String getTextAlert() {
+		return driver.switchTo().alert().getText();
+	}
+
+	public void sendkeysAlert(String keyVal) {
+		driver.switchTo().alert().sendKeys(keyVal);
+	}
+
+	public void clickToElement(By by) {
+		element = driver.findElement(by);
+		element.click();
+	}
+
+	public void sendkeysToElement(By by, String keyVal) {
+		element = driver.findElement(by);
+		element.clear();
+		element.sendKeys(keyVal);
+	}
+
+	public void selectItemInDropdown(By by, String itemVal) {
+		element = driver.findElement(by);
+		select = new Select(element);
+		select.selectByVisibleText(itemVal);
+	}
+
+	public String getItemValueInDropdown(By by) {
+		element = driver.findElement(by);
+		select = new Select(element);
+		return select.getFirstSelectedOption().getText();
+	}
+
+	public void selectItemInCustomDropdown(By parentXpath, By allItemXpath, String expectedItem) throws Exception {
+
+		WebElement parentDropdown = driver.findElement(parentXpath);
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", parentDropdown);
+		sleepInSecond(1);
+		jsExecutor.executeScript("arguments[0].click();", parentDropdown);
+		sleepInSecond(1);
+		waitExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(allItemXpath));
+		elements = driver.findElements(allItemXpath);
+		for (WebElement item : elements) {
+			if (item.getText().equals(expectedItem)) {
+				jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
+				sleepInSecond(1);
+				System.out.println("Item cần click : " + item.getText());
+				item.click();
+				sleepInSecond(2);
+				break;
+			}
+		}
+
+	}
+
+	public void sleepInSecond(long numberInSecond) {
+		try {
+			Thread.sleep(numberInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getAttributeValue(By by, String attributeName) {
+		element = driver.findElement(by);
+		return element.getAttribute(attributeName);
+	}
+
+	public String getTextElement(By by) {
+		element = driver.findElement(by);
+		return element.getText();
+	}
+
+	public int countElementNumber(By by) {
+		elements = driver.findElements(by);
+		return elements.size();
+	}
+
+	public void checkToCheckbox(By by) {
+		element = driver.findElement(by);
+		if (element.isSelected() == false) {
+			element.click();
+		}
+	}
+
+	public void unCheckToCheckbox(By by) {
+		element = driver.findElement(by);
+		if (element.isSelected() == true) {
+			element.click();
+		}
+	}
+
+	public boolean elementIsDisplayed(By by) {
+		element = driver.findElement(by);
+		return element.isDisplayed();
+	}
+
+	public boolean elementIsSelected(By by) {
+		element = driver.findElement(by);
+		return element.isSelected();
+	}
+
+	public boolean elementIsEnable(By by) {
+		element = driver.findElement(by);
+		return element.isEnabled();
+	}
+
+	public void switchToChildWindowsByID(String parentID) {
+		Set<String> allWindowsID = driver.getWindowHandles();
+		for (String currentWindowID : allWindowsID) {
+			if (!currentWindowID.equals(parentID)) {
+				driver.switchTo().window(currentWindowID);
+				break;
+			}
+		}
+	}
+
+	public void switchToWindowByTitle(String targerTitle) {
+		Set<String> allWindowsID = driver.getWindowHandles();
+		for (String currentWWindowID : allWindowsID) {
+			driver.switchTo().window(currentWWindowID);
+			String currentTitle = driver.getTitle();
+			if (targerTitle.equals(currentTitle)) {
+				break;
+			}
+		}
+	}
+
+	public boolean closeAllWindowsExceptParentWindows(String parentWindowsID) {
+		Set<String> allWindowsID = driver.getWindowHandles();
+		for (String currentWWindowID : allWindowsID) {
+			if (!parentWindowsID.equals(currentWWindowID)) {
+				driver.switchTo().window(currentWWindowID);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentWindowsID);
+		if (driver.getWindowHandles().size() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void switchToFrameOrIframe(By by) {
+		element = driver.findElement(by);
+		driver.switchTo().frame(element);
+	}
+
+	public void switchToParentPage(By by) {
+		driver.switchTo().defaultContent();
+	}
+
+	public void doubleClickToElement(By by) {
+		element = driver.findElement(by);
+		actions.doubleClick(element).perform();
+	}
+
+	public void hoverMouseToElement(By by) {
+		element = driver.findElement(by);
+		actions.moveToElement(element).perform();
+	}
+
+	public void rightClick(By by) {
+		element = driver.findElement(by);
+		actions.contextClick(element).perform();
+	}
+
+	public void dragAnđrop(By fromX, By toX) {
+		WebElement from = driver.findElement(fromX);
+		WebElement to = driver.findElement(toX);
+		actions.dragAndDrop(from, to).build().perform();
+	}
+
+	public void sendKeyBoardToElement(By by, Keys keysToSend) {
+		element = driver.findElement(by);
+		actions.sendKeys(element, keysToSend).perform();
+	}
+
+	public void clickAndHoldElement(By by, int from, int to) {
+		elements = driver.findElements(by);
+		actions.clickAndHold(elements.get(from)).moveToElement(elements.get(to)).release().perform();
+	}
+
+	public void clickAndSelectElement(By by, int elementLocator) {
+		elements = driver.findElements(by);
+		actions.keyDown(Keys.CONTROL).perform();
+		elements.get(elementLocator).click();
+		actions.keyUp(Keys.CONTROL).perform();
+	}
+
+	public void uploadFileBySenkeyCommands(By by, String filePath) {
+		element = driver.findElement(by);
+		element.sendKeys(filePath);
+	}
+
+	public void clickStartToUploadFile(By by) {
+		elements = driver.findElements(by);
+		for (WebElement startBtn : elements) {
+			startBtn.click();
+			sleepInSecond(2);
+		}
+	}
+
+	public void refreshBrowserByJS() {
+		jsExecutor.executeScript("history.go(0)");
+	}
+
+	public void navigateToPage(String pageUrl) {
+		jsExecutor.executeScript("window.location = \'" + pageUrl + "\'");
+	}
+
+	public boolean verifyTextInInnerText(String textExpected) {
+		String textActual = (String) jsExecutor
+				.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0]");
+		System.out.println("Text actual = " + textActual);
+		return textActual.equals(textExpected);
+	}
+
+	public String getPageTitleByJS() {
+		return jsExecutor.executeScript("return document.title").toString();
+	}
+
+	public String getPageUrlByJS() {
+		return jsExecutor.executeScript("return document.URL").toString();
+	}
+
+	public String getPageDomainByJS() {
+		return jsExecutor.executeScript("return document.domain").toString();
+	}
+
+	public void scrollToPixelByJS(int range) {
+		jsExecutor.executeScript("window.scrollBy(0," + range + ")");
+	}
+
+	public void scrollToBottomPageByJS() {
+		jsExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight");
+	}
+
+	public void scrollToDownPageByJS(int range) {
+		jsExecutor.executeScript("window.scrollBy(0," + range + ")", "");
+	}
+
+	public void clickToElementByJS(By by) {
+		element = driver.findElement(by);
+		jsExecutor.executeScript("arguments[0].click();", element);
+	}
+
+	public void highlightToElement(By by) {
+		element = driver.findElement(by);
+		jsExecutor.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');",
+				element);
+	}
+
+	public void scrollToElementByJS(By by) {
+		element = driver.findElement(by);
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+
+	public void removeAttributeInDOMByJS(By by, String attributeRemove) {
+		element = driver.findElement(by);
+		jsExecutor.executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", element);
+	}
+
+	public void sendkeyToElementByJS(By by, String value) {
+		element = driver.findElement(by);
+		jsExecutor.executeScript("arguments[0].setAttribute('value', '" + value + "')", element);
+	}
+
+	public boolean checkAnyImageLoaded(By by) {
+		element = driver.findElement(by);
+		boolean status = (boolean) jsExecutor.executeScript(
+				"return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0",
+				element);
+		if (status) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void waitForElementVisible(By by) {
+		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(by));
+	}
+
+	public void waitForElementInvisible(By by) {
+		waitExplicit.until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+
+	public void waitForElementPresense(By by) {
+		waitExplicit.until(ExpectedConditions.presenceOfElementLocated(by));
+	}
+
+	public void waitForElementClickable(By by) {
+		waitExplicit.until(ExpectedConditions.elementToBeClickable(by));
+	}
 }
---
+++ testcases/com/nopcommerce/account/Level_02_AbstractPage.java
@@ -0,0 +1,120 @@
+package com.nopcommerce.account;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import commons.AbstractPage;
+
+public class Level_02_AbstractPage {
+
+	WebDriver driver;
+	Select select;
+	String regFirstName, regLastName, regDateOfBirth, regDateOfMonth, regDateOfYear, regEmail, regCompanyName,
+			regPassword;
+	AbstractPage absPage;
+
+	By registerLinkX = By.xpath("//a[@class='ico-register']");
+	By registerPageX = By.xpath("//div[@class='page registration-page']");
+	By genderMaleRadioBtnX = By.xpath("//input[@id='gender-male']");
+	By firstNameTxtX = By.xpath("//input[@id='FirstName']");
+	By lastNameTxtX = By.xpath("//input[@id='LastName']");
+	By dateOfBirthDropDownX = By.xpath("//select[@name='DateOfBirthDay']");
+	By dateOfMonthDropDownX = By.xpath("//select[@name='DateOfBirthMonth']");
+	By dateOfYearDropDownX = By.xpath("//select[@name='DateOfBirthYear']");
+	By emailTxtX = By.xpath("//input[@id='Email']");
+	By companyNameTxtX = By.xpath("//input[@id='Company']");
+	By passwordTxtX = By.xpath("//input[@id='Password']");
+	By confirmPasswordTxtX = By.xpath("//input[@id='ConfirmPassword']");
+	By registerBtnX = By.xpath("//input[@id='register-button']");
+
+	By registerSuccessMsgX = By.xpath("//div[@class='result']");
+	By logOutLinkX = By.xpath("//a[@class='ico-logout']");
+
+	By loginLinkX = By.xpath("//a[@class='ico-login']");
+	By loginPageX = By.xpath("//div[@class='page login-page']");
+	By loginBtnX = By.xpath("//input[@class='button-1 login-button']");
+	By myAccountLinkX = By.xpath("//a[@class='ico-account']");
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		absPage = new AbstractPage(driver);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		regFirstName = "John";
+		regLastName = "Wick";
+		regDateOfBirth = "15";
+		regDateOfMonth = "May";
+		regDateOfYear = "1990";
+		regEmail = "john_wick_" + randomNumber() + "@gmail.com";
+		regCompanyName = "John Wick Entertainment";
+		regPassword = "123456";
+
+		String pageUrl = "https://demo.nopcommerce.com/";
+		absPage.openUrl(pageUrl);
+		
+	}
+
+	@Test
+	public void TC01_Register() {
+
+		absPage.clickToElement(registerLinkX);
+
+		absPage.clickToElement(genderMaleRadioBtnX);
+		absPage.sendkeysToElement(firstNameTxtX, regFirstName);
+		absPage.sendkeysToElement(lastNameTxtX, regLastName);
+		absPage.selectItemInDropdown(dateOfBirthDropDownX, regDateOfBirth);
+		absPage.selectItemInDropdown(dateOfMonthDropDownX, regDateOfMonth);
+		absPage.selectItemInDropdown(dateOfYearDropDownX, regDateOfYear);
+		absPage.sendkeysToElement(emailTxtX, regEmail);
+		absPage.sendkeysToElement(companyNameTxtX, regCompanyName);
+		absPage.sendkeysToElement(passwordTxtX, regPassword);
+		absPage.sendkeysToElement(confirmPasswordTxtX, regPassword);
+		absPage.clickToElement(registerBtnX);
+
+		System.out.println("TC01 : 2. Register message : " + absPage.getTextElement(registerSuccessMsgX));
+
+		System.out.println("TC01 : 3. Click Log Out Link");
+		absPage.clickToElement(logOutLinkX);
+
+		System.out.println("TC01 : 4. Navigate to Home Page : " + absPage.getPageCurrentUrl());
+
+	}
+
+	@Test
+	public void TC02_Login() {
+
+		absPage.clickToElement(loginLinkX);
+
+		System.out.println("TC02 : 2. Login Page Display Status :" + absPage.elementIsDisplayed(loginPageX));
+
+		absPage.sendkeysToElement(emailTxtX, regEmail);
+		absPage.sendkeysToElement(passwordTxtX, regPassword);
+		absPage.clickToElement(loginBtnX);
+
+		// Verify Login Successfully
+		System.out.println("TC02 : 3. Login Successfully Status : " + absPage.elementIsDisplayed(myAccountLinkX));
+	}
+
+	public int randomNumber() {
+		Random random = new Random();
+		return random.nextInt(999999);
+	}
+
+	@AfterClass
+	public void afterClass() {
+
+		System.out.println("Post-condition: Close Firefox browser");
+		driver.quit();
+	}
+
+}
